51cc162b6c188c59fd8b69f6b0ccea5e57a63ec3
saurav90 <saurav.s@dotw.com>
2019-12-12 18:16:26 +0530

    registration bug fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,11 +9,11 @@
         <div class="card">
             <div class="card-body register-card-body">
                 <p class="login-box-msg">Register a new membership</p>
+
                 <form action="{{ route('register') }}" method="post">
+                    @csrf
                     <div class="input-group mb-3">
-                        <input type="text" placeholder="Name" id="name" type="text"
-                               class="form-control @error('name') is-invalid @enderror" name="name"
-                               value="{{ old('name') }}" required autocomplete="name" autofocus>
+                        <input type="text" placeholder="Name" id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-user"></span>
@@ -21,23 +21,21 @@
                         </div>
                     </div>
                     <div class="input-group mb-3">
-                        <input id="email" placeholder="Email" type="email"
-                               class="form-control @error('email') is-invalid @enderror" name="email"
-                               value="{{ old('email') }}" required autocomplete="email">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
-                            </div>
+                        <input id="email"  placeholder="Email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
                         </div>
                     </div>
-                    @error('email')
-                    <span class="invalid-feedback" role="alert">
+                    </div>
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                    @enderror
+                        @enderror
+
                     <div class="input-group mb-3">
-                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation"
-                               required autocomplete="new-password" placeholder="Password">
+                        <input id="password-confirm" type="password" class="form-control" name="password" required autocomplete="new-password" placeholder="Password">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-lock"></span>
@@ -45,8 +43,7 @@
                         </div>
                     </div>
                     <div class="input-group mb-3">
-                        <input id="password-confirm" type="password" placeholder="Retype password" class="form-control"
-                               name="password_confirmation" required autocomplete="new-password">
+                        <input id="password-confirm" type="password"  placeholder="Retype password" class="form-control" name="password_confirmation" required autocomplete="new-password">
 
                         <div class="input-group-append">
                             <div class="input-group-text">
@@ -76,4 +73,135 @@
         </div><!-- /.card -->
     </div>
     <!-- /.register-box -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{{--<div class="container">--}}
+    {{--<div class="row justify-content-center">--}}
+        {{--<div class="col-md-8">--}}
+            {{--<div class="card">--}}
+                {{--<div class="card-header">{{ __('Register') }}</div>--}}
+
+                {{--<div class="card-body">--}}
+                    {{--<form method="POST" action="{{ route('register') }}">--}}
+                        {{--@csrf--}}
+
+                        {{--<div class="form-group row">--}}
+                            {{--<label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>--}}
+
+                            {{--<div class="col-md-6">--}}
+                                {{--<input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>--}}
+
+                                {{--@error('name')--}}
+                                    {{--<span class="invalid-feedback" role="alert">--}}
+                                        {{--<strong>{{ $message }}</strong>--}}
+                                    {{--</span>--}}
+                                {{--@enderror--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="form-group row">--}}
+                            {{--<label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>--}}
+
+                            {{--<div class="col-md-6">--}}
+                                {{--<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">--}}
+
+                                {{--@error('email')--}}
+                                    {{--<span class="invalid-feedback" role="alert">--}}
+                                        {{--<strong>{{ $message }}</strong>--}}
+                                    {{--</span>--}}
+                                {{--@enderror--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="form-group row">--}}
+                            {{--<label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>--}}
+
+                            {{--<div class="col-md-6">--}}
+                                {{--<input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">--}}
+
+                                {{--@error('password')--}}
+                                    {{--<span class="invalid-feedback" role="alert">--}}
+                                        {{--<strong>{{ $message }}</strong>--}}
+                                    {{--</span>--}}
+                                {{--@enderror--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="form-group row">--}}
+                            {{--<label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>--}}
+
+                            {{--<div class="col-md-6">--}}
+                                {{--<input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="form-group row mb-0">--}}
+                            {{--<div class="col-md-6 offset-md-4">--}}
+                                {{--<button type="submit" class="btn btn-primary">--}}
+                                    {{--{{ __('Register') }}--}}
+                                {{--</button>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</form>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</div>--}}
 @endsection
