98ed64adea6783f000118f23e4ca3fba0171cea5
saurav90 <saurav.s@dotw.com>
2020-01-08 19:22:45 +0530

    sortable complete + remove button addition

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,53 +1,56 @@
-$( function() {
-    $( "#sortable" ).sortable({
+var counter = 2;
+$(function () {
+    $("#sortable").sortable({
         stop: function (event, ui) {
             updateNames($(this))
-        }
+        },axis: 'y',containment: "parent"
     });
-    $( "#sortable" ).disableSelection();
-} );
+    $("#sortable").disableSelection();
+});
+
 function updateNames($list) {
     $list.find('.fieldSort').each(function (idx) {
         var $inp = $(this).find('input');
-        idx = idx+1;
+        idx = idx + 2;
         console.log($inp);
         $inp.each(function () {
             this.name = this.name.replace(/(\[\d\])/, '[' + idx + ']');
             this.id = this.id.replace(/(\[\d\])/, '[' + idx + ']');
-            $(this).parent().find('.col').text('Coloumn Name ' +idx);
-            $(this).parent().find('.desc').text('Coloumn Description ' +idx);
+            $(this).parent().find('.col').text('Coloumn Name ' + idx);
+            $(this).parent().find('.desc').text('Coloumn Description ' + idx);
             /* demo fn to show name as value*/
             showNameAsValue(this)
         })
 
     });
 }
+
 function showNameAsValue(el) {
     el.value = el.name;
 }
-var counter = 2;
-function appendThing() {
 
-    $("#sortable").append('<div class="row field-box fieldSort"><span class="fa fa-close delete">X</span> \n' +
+function appendThing() {
+    $("#sortable").append('<div class="row field-box fieldSort">\n' +
+        '                                        <div class="col-sm-12 clearfix" style="text-align: right; color: red;font-size:20px"><span class="delete"><i title = "Remove Coloumn " class="fa fa-window-close" aria-hidden="true"></i></span></div>' +
         '                                        <div class="col-sm-6">\n' +
         '                                            <div class="form-group">\n' +
-        '                                                <label for="exampleInputEmail1" class="col">Coloumn Name '+counter+'</label>\n' +
+        '                                                <label for="exampleInputEmail1" class="col">Coloumn Name ' + counter + '</label>\n' +
         '                                                <input type="text" class="form-control Cname" id="Cname[' + counter + ']" placeholder="Coloumn Name" name="Cname[' + counter + ']">\n' +
         '                                            </div>\n' +
         '                                        </div>\n' +
         '                                        <div class="col-sm-6">\n' +
         '                                            <div class="form-group">\n' +
-        '                                                <label for="exampleInputEmail1" class="desc">Coloumn Description '+counter+'</label>\n' +
+        '                                                <label for="exampleInputEmail1" class="desc">Coloumn Description ' + counter + '</label>\n' +
         '                                                <input type="text" class="form-control Cdesc" id="Cdesc[' + counter + ']" placeholder="Coloumn Description" name="Cdesc[' + counter + ']">\n' +
         '                                            </div>\n' +
         '                                        </div>\n' +
-        '                                    </div>');
+        '                    </div>');
 
-    $("#sortable").sortable({ refresh: sortable });
     counter++;
 };
 
 $(document).on('click', '.delete', function () {
-    $(this).parent().remove();
-    $("#sortable").sortable({ refresh: sortable });
+    $(this).parent().parent().remove();
+    updateNames($("#sortable"));
+    counter--;
 });
--- resources/views/suitemodels/addsuitemodel.blade.php
+++ resources/views/suitemodels/addsuitemodel.blade.php
@@ -22,23 +22,21 @@
                                 <div class="form-group">
                                     <button type="button" class="btn btn-block btn btn-info" onclick="appendThing()">Add Suit Coloumns </button>
                                 </div>
-                                <div  id="sortable">
-                                    <div class="row field-box fieldSort">
-                                        <div class="col-sm-6">
-                                            <div class="form-group">
-                                                <label for="exampleInputEmail1" class="col">Coloumn Name 1</label>
-                                                <input type="text" class="form-control Cname" id="Cname[0]" placeholder="Coloumn Name" name="Cname[0]">
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <div class="form-group">
-                                                <label for="exampleInputEmail1" class="desc">Coloumn Description 1</label>
-                                                <input type="text" class="form-control Cdesc" id="Cdesc[0]" placeholder="Coloumn Description" name="Cdesc[0]">
-                                            </div>
+                                <div class="row field-box">
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1" >Coloumn Name 1</label>
+                                            <input type="text" class="form-control Cname" id="Cname[0]" placeholder="Coloumn Name" name="Cname[0]">
                                         </div>
                                     </div>
-
-
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1" >Coloumn Description 1</label>
+                                            <input type="text" class="form-control Cdesc" id="Cdesc[0]" placeholder="Coloumn Description" name="Cdesc[0]">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div  id="sortable">
                                 </div>
                             </div>
                             <!-- /.card-body -->
