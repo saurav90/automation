85679f137af62d5e240395e83eba5b9dd487ef94
saurav90 <saurav.s@dotw.com>
2020-01-08 18:22:45 +0530

    commented code

--- resources/views/layouts/nav-top.blade.php
+++ resources/views/layouts/nav-top.blade.php
@@ -37,7 +37,7 @@
                 <a href="#" class="dropdown-item">
                     <!-- Message Start -->
                     <div class="media">
-                        <img src="{{ asset('dist/img/user1-128x128.jpg') }}" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+                        {{--<img src="{{ asset('dist/img/user1-128x128.jpg') }}" alt="User Avatar" class="img-size-50 mr-3 img-circle"/>--}}
                         <div class="media-body">
                             <h3 class="dropdown-item-title">
                                 Brad Diesel
@@ -53,7 +53,7 @@
                 <a href="#" class="dropdown-item">
                     <!-- Message Start -->
                     <div class="media">
-                        <img src="{{ asset('dist/img/user8-128x128.jpg') }}" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                        {{--<img src="{{ asset('dist/img/user8-128x128.jpg') }}" alt="User Avatar" class="img-size-50 img-circle mr-3">--}}
                         <div class="media-body">
                             <h3 class="dropdown-item-title">
                                 John Pierce
@@ -69,7 +69,7 @@
                 <a href="#" class="dropdown-item">
                     <!-- Message Start -->
                     <div class="media">
-                        <img src="{{ asset('dist/img/user3-128x128.jpg') }}" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                        {{--<img src="{{ asset('dist/img/user3-128x128.jpg') }}" alt="User Avatar" class="img-size-50 img-circle mr-3">--}}
                         <div class="media-body">
                             <h3 class="dropdown-item-title">
                                 Nora Silvester
@@ -133,11 +133,6 @@
                 </div>
             </div>
         </li>
-        {{--<li class="nav-item">--}}
-            {{--<a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#">--}}
-                {{--<i class="fas fa-th-large"></i>--}}
-            {{--</a>--}}
-        {{--</li>--}}
     </ul>
 </nav>
 <!-- /.navbar -->
